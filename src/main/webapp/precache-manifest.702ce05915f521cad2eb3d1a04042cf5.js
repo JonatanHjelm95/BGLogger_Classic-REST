8efe3efdfd7144aac4bfd3d0f89fc0ce3eca7ae4
JonatanHjelm95 <cph-jh409@cphbusiness.dk>
2020-06-06 10:10:58 +0200

    removed cettia package

---
+++ src/main/webapp/precache-manifest.702ce05915f521cad2eb3d1a04042cf5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c24d135e04f8050feaf8",
+    "url": "./static/css/main.a8475bf1.chunk.css"
+  },
+  {
+    "revision": "c24d135e04f8050feaf8",
+    "url": "./static/js/main.51cd649d.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "282fb8f7c5e7258dde2b",
+    "url": "./static/js/2.7c4ce8bf.chunk.js"
+  },
+  {
+    "revision": "a0f5efaa7411b87b760bbb475b17b06e",
+    "url": "./index.html"
+  }
+];
